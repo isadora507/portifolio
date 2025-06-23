f41922edf6e6f82f0721cd3e3efb34416709dcba
isadora507 <162143724+isadora507@users.noreply.github.com>
2025-06-23 12:28:42 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,18 @@
+const criarCard = [
+    {
+      categoria: "programação",
+      pergunta: "O que é Javascript?",
+      resposta: "Javascript é uma linguagem de programação"
+    },
+    {
+      categoria: "geografia",
+      pergunta: "Qual o estado do Paraná?",
+      resposta: "A capital do Paraná é Curitiba"
+    },
+    {
+      categoria: "programação",
+      pergunta: "O que é HTML?",
+      resposta: "HTML é uma linguagem de marcação"
+    }
+  ];
+  
